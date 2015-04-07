19d5dcfc69f6464cab939c39881a9d40a6a20651
A. Potato <tanlk99@gmail.com>
2015-04-07 17:28:03 +0800

    Second.8 Commit

--- app/src/main/java/com/example/s9941643b/homeworktracker/HomeworkContent.java
+++ app/src/main/java/com/example/s9941643b/homeworktracker/HomeworkContent.java
@@ -14,6 +14,17 @@ public class HomeworkContent {
     public static List<Homework> ITEMS = new ArrayList<Homework>();
     public static Map<String, Homework> ITEM_MAP = new HashMap<String, Homework>();
 
+    static {
+        addItem(new Homework("Weed Agriculture", "Biology", new Date(1000000000), new Date(1000000000)));
+        addItem(new Homework("Eating Food", "Biology", new Date(1000000000), new Date(1000000000)));
+        addItem(new Homework("Sleeping", "Neuroscience", new Date(1000000000), new Date(1000000000)));
+    }
+
+    private static void addItem(Homework item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.mName, item);
+    }
+
     public static class Homework {
         private String mName, mSubject;
         private Date mDateDue, mDateRemind;
