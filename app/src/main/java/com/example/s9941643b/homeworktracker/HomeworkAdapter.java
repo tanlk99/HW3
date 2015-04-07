4828c7209ddf45651fc8fe168b001cc41802efcb
A. Potato <tanlk99@gmail.com>
2015-04-07 17:04:23 +0800

    Second.6 Commit

--- app/src/main/java/com/example/s9941643b/homeworktracker/HomeworkAdapter.java
+++ app/src/main/java/com/example/s9941643b/homeworktracker/HomeworkAdapter.java
@@ -5,17 +5,22 @@ package com.example.s9941643b.homeworktracker;
  */
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.s9941643b.homeworktracker.HomeworkContent.Homework;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomeworkAdapter extends ArrayAdapter<Homework> {
+    private Context mContext;
     private int mResource;
-    private List<Homework> mHomeworkList;
+    private List<Homework> mHomeworkList = new ArrayList<Homework>;
 
     public HomeworkAdapter(Context context, int resource, List<Homework> homeworkList) {
         super(context, resource, homeworkList);
@@ -25,8 +30,21 @@ public class HomeworkAdapter extends ArrayAdapter<Homework> {
 
     @Override
     public View getView(int position, View row, ViewGroup parent) {
+        HomeworkHolder holder = null;
+
         if (row == null) {
-            row = getLayoutInflater().inflate(mResource, parent, false);
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            row = inflater.inflate(mResource, parent, false);
         }
+        else {
+            holder = (HomeworkHolder)row.getTag();
+        }
+        Homework currentHomework = mHomeworkList.get(position);
+        return row;
+    }
+
+    static class HomeworkHolder {
+        private TextView mNameView, mSubjectView, mDateDueView;
+        private ImageView mDoneIcon;
     }
 }
--- app/src/main/java/com/example/s9941643b/homeworktracker/HomeworkListFragment.java
+++ app/src/main/java/com/example/s9941643b/homeworktracker/HomeworkListFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-
 import com.example.s9941643b.homeworktracker.dummy.DummyContent;
 
 /**
